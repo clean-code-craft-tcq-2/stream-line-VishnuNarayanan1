aea9bf5424f116821d172c8092f4b8e7d4543468
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-04-07 06:23:33 +0200

    Update Sender.c

--- Sender.c
+++ Sender.c
@@ -3,14 +3,42 @@
 void Interpret()
 {
   int loopIndex = 0;
-  float loadSensorOutput[50];
+  float loadSensorOutput;
   for(loopIndex = 0; loopIndex < MAX_BATTERY_PARAMETERS; loopIndex++)
   {
     if((BatteryParameters_st + loopIndex)->SensorReadFunc_fp != NULL)
     {
-       (BatteryParameters_st + loopIndex)->SensorReadFunc_fp(loadSensorOutput);
-       (BatteryParameters_st + loopIndex)->ProcessSensorValue_fp(loadSensorOutput);
-       (BatteryParameters_st + loopIndex)->PrintProcessedSensorValue_fp(loadSensorOutput);     
+       sensorValue = (BatteryParameters_st + loopIndex)->SensorReadFunc_fp();
+       processedSensorValue = (BatteryParameters_st + loopIndex)->ProcessSensorValue_fp(sensorValue);
+       (BatteryParameters_st + loopIndex)->PrintProcessedSensorValue_fp(processedSensorValue);     
     }
   }
 }
+
+float ReadTemperatureFromSensor(void)
+{
+   return TemperatureValue;
+}
+
+float ReadVoltage(void)
+{
+   return VoltageValue;
+}
+
+float ProcessTemperatureValue (float sensorValue)
+{
+  if(sensorValue > MIN_TEMP_LIMIT && sensorValue < MAX_TEMP_LIMIT)
+    return (sensorValue + TEMPERATURE_OFFSET);
+  else
+    return INVALID_TEMPERATURE;
+}
+
+float ProcessVoltage (float voltageValue)
+{
+  if(voltageValue > MIN_VOLTAGE_LIMIT && voltageValue < MAX_VOLTAGE_LIMIT)
+    return voltageValue;
+  else
+    return INVALID_VOLTAGE;
+}
+
+PrintInJSONFormat
