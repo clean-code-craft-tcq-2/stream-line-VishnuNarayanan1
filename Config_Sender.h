34d5cd838287cf0659f4b1f7cc3f15e128b219a0
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-04-07 06:31:16 +0200

    Update Config_Sender.h

--- Config_Sender.h
+++ Config_Sender.h
@@ -12,6 +12,7 @@
 typedef struct {
    float (*SensorReadFunc_fp)(void);  
    float (*ProcessSensorOutputFunc_fp)(float);
+   void (*PrintInConsole_fp)(int,float);
 } tst_ReadBatteryParameters;
 
 extern const tst_ReadBatteryParameters ReadBatteryParameters_st[MAX_BATTERY_PARAMETERS];
