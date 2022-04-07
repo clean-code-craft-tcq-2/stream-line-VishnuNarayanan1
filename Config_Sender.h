0a2ab3a97b0e393386fa461c817e0cee46425b60
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-04-07 06:23:06 +0200

    Update Config_Sender.h

--- Config_Sender.h
+++ Config_Sender.h
@@ -1,8 +1,13 @@
 #define MAX_BATTERY_PARAMETERS 2
+
 #define INVALID_TEMPERATURE 65535
 #define MIN_TEMP_LIMIT -10
 #define MAX_TEMP_LIMIT  50
 #define TEMPERATURE_OFFSET 0.75
+
+#define INVALID_VOLTAGE 0
+#define MIN_VOLTAGE_LIMIT 2.5
+#define MAX_VOLTAGE_LIMIT 12
 
 typedef struct {
    float (*SensorReadFunc_fp)(void);  
