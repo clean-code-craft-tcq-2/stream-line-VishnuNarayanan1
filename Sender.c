a37be328336b67770c045707e5573c0baa47ae22
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-04-07 07:55:52 +0200

    Update Sender.c

--- Sender.c
+++ Sender.c
@@ -45,5 +45,5 @@ float ProcessVoltage (float voltageValue)
 
 void PrintInJSONFormat(int batteryParameter, float processedSensorValue)
 {
-   printf("%s %.1f", BatteryParametersJSONString[batteryParameter] , processedSensorValue);
+   printf("%s %.1f\n", BatteryParametersJSONString[batteryParameter] , processedSensorValue);
 }
