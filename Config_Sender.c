9621169862d3ca7e79859d4ca3d622749822fe39
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-04-07 05:39:29 +0200

    Update Config_Sender.c

--- Config_Sender.c
+++ Config_Sender.c
@@ -1,7 +1,10 @@
 const tst_ReadBatteryParameters ReadBatteryParameters_st[MAX_BATTERY_PARAMETERS] =
 {
-    {&ReadTemperatureFromSensor},
-    {&ReadChargeRateFromSensor}
+    {&ReadTemperatureFromSensor, &ProcessTemperatureValue, &PrintInJSONFormat},
+    {&ReadChargeRateFromSensor, &ProcessChargeRate, &PrintInJSONFormat}
 };
+
+char BatteryParametersJSONString[MAX_BATTERY_PARAMETERS][50] =
+{"Temperature : ","ChargeRate : "};
 
 
