0268d1c92c08a087e1e02dc5c2d06552bd6fcba2
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-04-07 08:57:37 +0200

    Update Sender.c

--- Sender.c
+++ Sender.c
@@ -2,19 +2,22 @@
 #include "Config_Sender.h"
 #include "Sender.h"
 
-void ReadAndSendBatteryParameters(void)
+int ReadAndSendBatteryParameters(void)
 {
   int batteryParameterIndex = 0;
+  int functionExecutionStatus = 0;
   float sensorValue,processedSensorValue;
   for(batteryParameterIndex = 0; batteryParameterIndex < MAX_BATTERY_PARAMETERS; batteryParameterIndex++)
   {
+    functionExecutionStatus = 0;
     if((ReadBatteryParameters_st + batteryParameterIndex)->ReadSensorFunc_fp != NULL)
     {
        sensorValue = (ReadBatteryParameters_st + batteryParameterIndex)->ReadSensorFunc_fp();
        processedSensorValue = (ReadBatteryParameters_st + batteryParameterIndex)->ProcessSensorValue_fp(sensorValue);
-       (ReadBatteryParameters_st + batteryParameterIndex)->PrintInConsole_fp(batteryParameterIndex, processedSensorValue);     
+       functionExecutionStatus = (ReadBatteryParameters_st + batteryParameterIndex)->PrintInConsole_fp(batteryParameterIndex, processedSensorValue);     
     }
   }
+  return functionExecutionStatus;
 }
 
 float ReadTemperatureFromSensor(void)
@@ -43,7 +46,8 @@ float ProcessVoltage (float voltageValue)
     return INVALID_VOLTAGE;
 }
 
-void PrintInJSONFormat(int batteryParameter, float processedSensorValue)
+int PrintInJSONFormat(int batteryParameter, float processedSensorValue)
 {
    printf("%s %.1f\n", BatteryParametersJSONString[batteryParameter] , processedSensorValue);
+   return 1;
 }
