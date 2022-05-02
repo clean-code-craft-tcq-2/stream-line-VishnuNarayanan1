7be214f03ab5455cbe7e4fac7cb26bfc69b243fb
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2022-05-02 20:54:24 +0530

    Delete main.c

--- Receiver/main.c
+++
@@ -1,9 +0,0 @@
-#include "receiver.h"
-
-int main()
-{
-    float Temperature[50], Voltage[50] = {0};
-    readfromConsole(Temperature, Voltage, 50);  
-    FindMinandMaxValue(Temperature, Voltage, 50);
-    return 0;
-}
