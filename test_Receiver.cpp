ec834914ac03b84199b8f0682f23d9bc9c34aa49
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2022-05-05 12:33:20 +0530

    beautify test

--- test_Receiver.cpp
+++ test_Receiver.cpp
@@ -4,16 +4,30 @@
 #include "Receiver/receiver.h"
 
 /* Positive test case */
-TEST_CASE("Case for Postive test case :- To read Data from console") 
+TEST_CASE("Case for Postive test case 1 :- To read Data from console") 
 {
 	float temperature[1] = {0};
 	float voltage[1] = {0};
 	float temperature_ref[1] = {24.9};
 	float voltage_ref[1] = {0.0};
-    	
+    	printf("------ Data fetched by receiver from sender ------\n");
+	readfromConsole(temperature, voltage, 1);
+	printf("------ Receiver reference verification data ------\n");
 	printf(" Temperature: %.2f , Voltage: %.2f \n", temperature_ref[0],  voltage_ref[0]);
-	printf("Sender fetched data\n");
-  readfromConsole(temperature, voltage, 1);
+  REQUIRE(temperature[0] - temperature_ref[0] <= 0.001);
+  REQUIRE(voltage[0] - voltage_ref[0] <= 0.001);
+}
+
+TEST_CASE("Case for Postive test case 2 :- To read Data from console") 
+{
+	float temperature[1] = {0};
+	float voltage[1] = {0};
+	float temperature_ref[7] = {25.5};
+	float voltage_ref[7] = {3.1};
+	printf("------ Data fetched by receiver from sender ------\n");
+	readfromConsole(temperature, voltage, 7);
+    	printf("------ Receiver reference verification data ------\n");
+	printf(" Temperature: %.2f , Voltage: %.2f \n", temperature_ref[0],  voltage_ref[0]);
   REQUIRE(temperature[0] - temperature_ref[0] <= 0.001);
   REQUIRE(voltage[0] - voltage_ref[0] <= 0.001);
 }
